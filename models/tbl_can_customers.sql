02fd2c322088b993945644cb71360042ce82ad9a
shaik mahammad <smahammad880@gmail.com>
2026-08-11 14:54:36 +0000

    modified vw_can_customers.sql and source file

--- models/tbl_can_customers.sql
+++ models/tbl_can_customers.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized='table',
+        transient=false
+    )
+}}
 select c_custkey,c_name,c_address from {{ source('s1', 'tbl_customer') }}
 inner join {{ source('s1', 'tbl_nation') }} on {{ source('s1', 'tbl_nation') }}.N_NATIONKEY={{ source('s1', 'tbl_customer') }}.C_NATIONKEY
-WHERE {{ source('s1', 'tbl_nation') }}.n_name='CANADA'
+WHERE {{ source('s1', 'tbl_nation') }}.n_name='CANADA' limit 190
