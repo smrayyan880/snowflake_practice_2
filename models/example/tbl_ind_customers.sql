f6c0afb13babcd4628efe143686e6ad4e8f42f0c
shaik mahammad <smahammad880@gmail.com>
2026-08-11 06:44:09 +0000

    created transient and parmenant table using queries

---
+++ models/example/tbl_ind_customers.sql
@@ -0,0 +1,10 @@
+
+{{
+    config(
+        materialized='table',
+        transient=false
+    )
+}}
+select c_custkey,c_name,c_address from dbt_customers.dbt_schema.tbl_customer
+inner join dbt_customers.dbt_schema.tbl_nation on tbl_nation.N_NATIONKEY=tbl_customer.C_NATIONKEY
+WHERE tbl_nation.n_name='INDIA'
